8458d60e40e8ba90b68ef2b3c8c00c36b2d47388
marselle.naz <101027445+marse11e@users.noreply.github.com>
2022-12-27 17:58:52 +0600

    Update API Rick.py

--- API Rick.py
+++ API Rick.py
@@ -6,40 +6,44 @@ def get_info(gets):
     data = response.json()
     return data
 
+def get_location(gets):
+    get_id = get_info(f'character/{gets}')
+    for i in get_id.keys():
+        if i == 'location':
+            name_location = get_id['location']['name']
+            url_location = get_id['location']['url']
+            if url_location not in '':
+                id_url = url_location.split('/')[-1]
+                location = get_info(f'location/{id_url}')
+                name_dimension = location['dimension']
+                type_location = location['type']
+                information_location = f"""
+        Название локации: {name_location}
+        Тип локации: {type_location}
+        Измерение локации: {name_dimension}"""
+                return information_location
+            else:
+                information_location = f"""
+        Название локации: {name_location}
+        Тип локации: Unknown
+        Измерение локации: Unknown"""
+                return information_location
 
 def get_character_info(gets):
     if gets <= 826:
         character = get_info(f'character/{gets}')
-        edit_sylka = character['location']['url']
-        if edit_sylka is not None:
-            id_locations = edit_sylka.split('/')[-1]
-            location = get_info(f'location/{id_locations}')
-            information = f"""
-            Идентификатор персонажа: {character['id']}
-            Имя персонажа: {character['name']}
-            Пол персонажа: {character['gender']}
-            Жизненное положение: {character['status']}
-            Какой расе относится: {character['species']}
-            Личность: {character['type']}
-            Имя локации: {character['location']['name']}
-            Тип локации: {location['type']}
-            Измерения: {location['dimension']}
-            Дата создание: {character['created']}
-            """
-            return information
-        elif edit_sylka is None:
-            information = f"""
-            Идентификатор персонажа: {character['id']}
-            Имя персонажа: {character['name']}
-            Пол персонажа: {character['gender']}
-            Жизненное положение: {character['status']}
-            Какой расе относится: {character['species']}
-            Личность: {character['type']}
-            Имя локации: {character['location']['name']}
-            Тип локации: Unknown
-            Измерения: Unknown
-            Дата создание: {character['created']}
-            """
-            return information
-        return 'Не правильный id персонажа'
-print(get_character_info(900))
+        information = f"""
+        Идентификатор персонажа: {character['id']}
+        Имя персонажа: {character['name']}
+        Пол персонажа: {character['gender']}
+        Жизненное положение: {character['status']}
+        Какой расе относится: {character['species']}
+        Личность: {character['type']}
+        Дата создание: {character['created']}
+        
+        {get_location(gets)}
+        """
+
+        return information
+    return 'Не правильный id персонажа'
+print(get_character_info(361))
